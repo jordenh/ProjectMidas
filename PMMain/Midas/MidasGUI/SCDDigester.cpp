ec54ff1318550007bcc8944b307606f651c4f025
Jorden Hetherington <jorden.hetherington@gmail.com>
2015-07-06 14:05:51 -0700

    added licensing statement to all source code files

--- PMMain/Midas/MidasGUI/SCDDigester.cpp
+++ PMMain/Midas/MidasGUI/SCDDigester.cpp
@@ -1,3 +1,22 @@
+/*
+    Copyright (C) 2015 Midas
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Lesser General Public
+    License as published by the Free Software Foundation; either
+    version 2.1 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public
+    License along with this library; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301
+    USA
+*/
+
 #include "SCDDigester.h"
 
 #include "BaseMeasurements.h"
@@ -16,7 +35,8 @@ SCDDigester::SCDDigester(SharedCommandData* scd, MidasThread *thread, ControlSta
 	MouseCtrl *mouseCtrl, KybrdCtrl *kybrdCtrl, KeyboardController *keyboardController, ProfileManager* profileManagerHandle, std::vector<ringData> *kybrdRingData)
 #else
 SCDDigester::SCDDigester(SharedCommandData* scd, MidasThread *thread, ControlState *cntrlStateHandle, MyoState* myoStateHandle,
-	MouseCtrl *mouseCtrl, KybrdCtrl *kybrdCtrl, KeyboardController *keyboardController, ProfileManager* profileManagerHandle)
+	MouseCtrl *mouseCtrl, KybrdCtrl *kybrdCtrl, KeyboardController *keyboardController, ProfileM
+    anager* profileManagerHandle)
 #endif
 {
     this->scdHandle = scd;
--- PMMain/Midas/MidasGUI/main.cpp
+++ PMMain/Midas/MidasGUI/main.cpp
@@ -1,5 +1,5 @@
 /*
-    Copyright (C) 2015 Jorden Hetherington
+    Copyright (C) 2015 Midas
 
     This library is free software; you can redistribute it and/or
     modify it under the terms of the GNU Lesser General Public
