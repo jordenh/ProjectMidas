21e30b8b975d7b720c5f8f8b27eb445fd2e47798
Jorden Hetherington <jorden.hetherington@gmail.com>
2015-08-21 16:13:42 -0700

    wrote src code for the myo status widget

---
+++ PMMain/Midas/MidasGUI/MyoStatusWidget.h
@@ -0,0 +1,61 @@
+/*
+    Copyright (C) 2015 Midas
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Lesser General Public
+    License as published by the Free Software Foundation; either
+    version 2.1 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public
+    License along with this library; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301
+    USA
+*/
+
+#ifndef BATTERY_WIDGET_H
+#define BATTERY_WIDGET_H
+
+#include "DraggableWidget.h"
+#include "MidasCommon.h"
+#include "MainGUI.h"
+
+class QVBoxLayout;
+class QProgressBar;
+class QLabel;
+
+class MyoStatusWidget : public DraggableWidget {
+    Q_OBJECT
+public:
+    MyoStatusWidget(int widgetWidth = BATTERY_WIDGET_WIDTH,
+        int widgetHeight = BATTERY_WIDGET_HEIGHT, QWidget *parent = 0);
+    ~MyoStatusWidget();
+
+    QSize sizeHint() const;
+
+public slots :
+    void handleBatteryLevelChange(int);
+
+    void handleSignalStrengthChange(int);
+
+private:
+    void updateGUI();
+
+    int indWidth, indHeight;
+
+    unsigned int batteryLevel;
+    int signalStrength;
+
+    QString lowValueStyle;
+    QString highValueStyle;
+
+    QVBoxLayout *layout;
+    QProgressBar *batteryLevelBar;
+    QProgressBar *signalStrengthBar;
+};
+
+#endif BATTERY_WIDGET_H
